7638a52153fa7f0eb448123fed136fc816a39c34
Jiezhi.Xiao <jzxiao@thoughtworks.com>
2020-05-13 21:42:41 +0800

    impl: scenario 1.1 common product, quality drop down to 0 after reaching expiration date

--- src/main/java/cn/xpbootcamp/gildedrose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gildedrose/GildedRose.java
@@ -3,22 +3,24 @@ package cn.xpbootcamp.gildedrose;
 public class GildedRose {
 
 
-    public Product updateSellinAndQualityAfterPassDay(Product product, int passedDay){
+    public Product updateSellinAndQualityAfterPassDay(Product product, int passDay) {
 
-        int sellIn = product.getSellIn();
+        int currentSellIn = product.getSellIn();
+        int currentQuality = product.getQuality();
 
-        if (product.getType() == Product.COMMON_PRODUCT_TYPE && sellIn > 0 && sellIn >= passedDay) {
-            int oldSellin = sellIn;
-            int oldQuality = product.getQuality();
-            product.setSellIn(oldSellin - passedDay);
-            product.setQuality(oldQuality - passedDay * 1);
+        if (product.getType().equals(Product.COMMON_PRODUCT_TYPE) && currentSellIn > 0 && currentSellIn >= passDay) {
+            product.setSellIn(currentSellIn - passDay);
+            product.setQuality(currentQuality - passDay);
         }
-        if (product.getType() == Product.COMMON_PRODUCT_TYPE && sellIn > 0 && sellIn < passedDay) {
 
-            int oldSellin = sellIn;
-            int oldQuality = product.getQuality();
-            product.setSellIn(oldSellin - passedDay);
-            product.setQuality(oldQuality - passedDay * 1);
+        if (product.getType().equals(Product.COMMON_PRODUCT_TYPE) && currentSellIn > 0 && currentSellIn < passDay) {
+            product.setSellIn(currentSellIn - passDay);
+
+            int reducedQualityInExpirationDate = currentSellIn * 1;
+            int reducedQualityBeyondExpirationDate = (passDay - currentSellIn) * 2;
+
+            int tmpQuality = currentQuality - reducedQualityInExpirationDate - reducedQualityBeyondExpirationDate;
+            product.setQuality(Math.max(tmpQuality, 0));
         }
         return product;
     }
--- src/test/java/cn/xpbootcamp/gildedrose/GildedRoseTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/GildedRoseTest.java
@@ -2,7 +2,8 @@ package cn.xpbootcamp.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GildedRoseTest {
     GildedRose rose = new GildedRose();
@@ -11,39 +12,39 @@ public class GildedRoseTest {
     public void should_return_product_with_29_quality_and_9_sellin_when_pass_1day_for_common_product_30_quality_and_10_sellin_in_expiration_date() {
         Product commonProduct = new Product("milk", Product.COMMON_PRODUCT_TYPE, 10, 30);
         Product commonProductAfterDays = rose.updateSellinAndQualityAfterPassDay(commonProduct, 1);
-        assertEquals(commonProductAfterDays.getQuality(), 29);
-        assertEquals(commonProductAfterDays.getSellIn(), 9);
+        assertThat(commonProductAfterDays.getQuality(), equalTo(29));
+        assertThat(commonProductAfterDays.getSellIn(), equalTo(9));
     }
 
     @Test
     public void should_return_product_with_20_quality_and_0_sellin_when_pass_10days_for_common_product_30_quality_and_10_sellin_and_selin_is_0() {
         Product commonProduct = new Product("milk", Product.COMMON_PRODUCT_TYPE, 10, 30);
-        Product commonProductAfter1Day = rose.updateSellinAndQualityAfterPassDay(commonProduct, 10);
-        assertEquals(commonProductAfter1Day.getQuality(), 20);
-        assertEquals(commonProductAfter1Day.getSellIn(), 0);
+        Product commonProductAfterDays = rose.updateSellinAndQualityAfterPassDay(commonProduct, 10);
+        assertThat(commonProductAfterDays.getQuality(), equalTo(20));
+        assertThat(commonProductAfterDays.getSellIn(), equalTo(0));
     }
 
     @Test
     public void should_return_product_with_19_quality_and_negative_1_sellin_when_pass_11days_for_common_product_30_quality_and_10_sellin_out_of_expiration_date() {
         Product commonProduct = new Product("milk", Product.COMMON_PRODUCT_TYPE, 10, 30);
-        Product commonProductAfter1Day = rose.updateSellinAndQualityAfterPassDay(commonProduct, 11);
-        assertEquals(commonProductAfter1Day.getQuality(), 19);
-        assertEquals(commonProductAfter1Day.getSellIn(), -1);
+        Product commonProductAfterDays = rose.updateSellinAndQualityAfterPassDay(commonProduct, 11);
+        assertThat(commonProductAfterDays.getQuality(), equalTo(18));
+        assertThat(commonProductAfterDays.getSellIn(), equalTo(-1));
     }
 
     @Test
     public void should_return_product_with_0_quality_and_negative_20_sellin_when_pass_30days_for_common_product_30_quality_and_10_sellin_out_of_expiration_date() {
         Product commonProduct = new Product("milk", Product.COMMON_PRODUCT_TYPE, 10, 30);
-        Product commonProductAfter1Day = rose.updateSellinAndQualityAfterPassDay(commonProduct, 30);
-        assertEquals(commonProductAfter1Day.getQuality(), 0);
-        assertEquals(commonProductAfter1Day.getSellIn(), -20);
+        Product commonProductAfterDays = rose.updateSellinAndQualityAfterPassDay(commonProduct, 30);
+        assertThat(commonProductAfterDays.getQuality(), equalTo(0));
+        assertThat(commonProductAfterDays.getSellIn(), equalTo(-20));
     }
 
     @Test
     public void should_return_product_with_0_quality_and_negative_21_sellin_when_pass_30days_for_common_product_31_quality_and_10_sellin_out_of_expiration_date() {
         Product commonProduct = new Product("milk", Product.COMMON_PRODUCT_TYPE, 10, 30);
-        Product commonProductAfter1Day = rose.updateSellinAndQualityAfterPassDay(commonProduct, 31);
-        assertEquals(commonProductAfter1Day.getQuality(), 0);
-        assertEquals(commonProductAfter1Day.getSellIn(), -21);
+        Product commonProductAfterDays = rose.updateSellinAndQualityAfterPassDay(commonProduct, 31);
+        assertThat(commonProductAfterDays.getQuality(), equalTo(0));
+        assertThat(commonProductAfterDays.getSellIn(), equalTo(-21));
     }
 }
