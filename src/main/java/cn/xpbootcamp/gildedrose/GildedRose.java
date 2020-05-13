dc21517b98b82ec363750c276121564bc0bed91b
Jiezhi.Xiao <jzxiao@thoughtworks.com>
2020-05-13 21:59:30 +0800

    impl: scenario 1.2, common product, quality drop down to 0 before reaching expiration date

--- src/main/java/cn/xpbootcamp/gildedrose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gildedrose/GildedRose.java
@@ -10,16 +10,18 @@ public class GildedRose {
 
         if (product.getType().equals(Product.COMMON_PRODUCT_TYPE) && currentSellIn > 0 && currentSellIn >= passDay) {
             product.setSellIn(currentSellIn - passDay);
-            product.setQuality(currentQuality - passDay);
+
+            int tmpQuality = currentQuality - passDay * 1;
+            product.setQuality(Math.max(tmpQuality, 0));
         }
 
         if (product.getType().equals(Product.COMMON_PRODUCT_TYPE) && currentSellIn > 0 && currentSellIn < passDay) {
             product.setSellIn(currentSellIn - passDay);
 
             int reducedQualityInExpirationDate = currentSellIn * 1;
-            int reducedQualityBeyondExpirationDate = (passDay - currentSellIn) * 2;
+            int reducedQualityOutOfExpirationDate = (passDay - currentSellIn) * 2;
 
-            int tmpQuality = currentQuality - reducedQualityInExpirationDate - reducedQualityBeyondExpirationDate;
+            int tmpQuality = currentQuality - reducedQualityInExpirationDate - reducedQualityOutOfExpirationDate;
             product.setQuality(Math.max(tmpQuality, 0));
         }
         return product;
--- src/test/java/cn/xpbootcamp/gildedrose/GildedRoseTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/GildedRoseTest.java
@@ -1,12 +1,18 @@
 package cn.xpbootcamp.gildedrose;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GildedRoseTest {
-    GildedRose rose = new GildedRose();
+    GildedRose rose;
+
+    @BeforeEach
+    public void setUp() {
+        rose = new GildedRose();
+    }
 
     @Test
     public void should_return_product_with_29_quality_and_9_sellin_when_pass_1day_for_common_product_30_quality_and_10_sellin_in_expiration_date() {
@@ -46,5 +52,32 @@ public class GildedRoseTest {
         Product commonProductAfterDays = rose.updateSellinAndQualityAfterPassDay(commonProduct, 31);
         assertThat(commonProductAfterDays.getQuality(), equalTo(0));
         assertThat(commonProductAfterDays.getSellIn(), equalTo(-21));
+    }
+
+    @Test
+    public void should_return_product_with_0_quality_and_10_sellin_when_pass_10_days_for_common_product_with_10_quality_and_20_sellin_in_expiration_date() {
+        Product product = new Product("B", Product.COMMON_PRODUCT_TYPE, 20, 10);
+        rose.updateSellinAndQualityAfterPassDay(product, 10);
+
+        assertThat(product.getQuality(), equalTo(0));
+        assertThat(product.getSellIn(), equalTo(10));
+    }
+
+    @Test
+    public void should_return_product_with_0_quality_and_5_sellin_when_pass_15_days_for_common_product_with_10_quality_and_20_sellin_in_expiration_date() {
+        Product product = new Product("B", Product.COMMON_PRODUCT_TYPE, 20, 10);
+        rose.updateSellinAndQualityAfterPassDay(product, 15);
+
+        assertThat(product.getQuality(), equalTo(0));
+        assertThat(product.getSellIn(), equalTo(5));
+    }
+
+    @Test
+    public void should_return_product_with_0_quality_and_negative_1_sellin_when_pass_21_days_for_common_product_with_10_quality_and_20_sellin_out_of_expiration_date() {
+        Product product = new Product("B", Product.COMMON_PRODUCT_TYPE, 20, 10);
+        rose.updateSellinAndQualityAfterPassDay(product, 21);
+
+        assertThat(product.getQuality(), equalTo(0));
+        assertThat(product.getSellIn(), equalTo(-1));
     }
 }
