6a2e11c8aae8db4a8e66b970c63212dccd9113f9
lixiang093214 <lixianggxu@163.com>
2020-05-16 22:39:12 +0800

    update: rename adebBrie test case

--- src/test/java/cn/xpbootcamp/gildedrose/AdedBrieProduct.java
+++ src/test/java/cn/xpbootcamp/gildedrose/AdedBrieProduct.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AdedBrieProductTest {
 
     @Test
-    void should_quality_31_and_sellin_29_when_one_day_pass_given_quality_30_and_sellin_30_of_common_product_in_expiration_date() {
+    void should_quality_31_and_sellin_29_when_one_day_pass_given_quality_30_and_sellin_30_of_adebBrie_product_in_expiration_date() {
         AdedBrieProduct adebBrie = new AdedBrieProduct(30, 30);
         adebBrie.passDay();
         assertEquals(31, adebBrie.getQuality());
@@ -15,7 +15,7 @@ class AdedBrieProductTest {
     }
 
     @Test
-    void should_quality_50_and_sellin_9_when_one_day_pass_given_quality_50_and_sellin_10_of_common_product_in_expiration_date() {
+    void should_quality_50_and_sellin_9_when_one_day_pass_given_quality_50_and_sellin_10_of_adebBrie_product_in_expiration_date() {
         AdedBrieProduct adebBrie = new AdedBrieProduct(50, 10);
         adebBrie.passDay();
         assertEquals(50, adebBrie.getQuality());
@@ -23,7 +23,7 @@ class AdedBrieProductTest {
     }
 
     @Test
-    void should_quality_32_and_sellin_negative_1_when_one_day_pass_given_quality_30_and_sellin_0_of_common_product_in_expiration_date() {
+    void should_quality_32_and_sellin_negative_1_when_one_day_pass_given_quality_30_and_sellin_0_of_adebBrie_product_in_expiration_date() {
         AdedBrieProduct adebBrie = new AdedBrieProduct(30, 0);
         adebBrie.passDay();
         assertEquals(32, adebBrie.getQuality());
@@ -31,7 +31,7 @@ class AdedBrieProductTest {
     }
 
     @Test
-    void should_quality_50_and_sellin_negative_1_when_one_day_pass_given_quality_50_and_sellin_0_of_common_product_in_expiration_date() {
+    void should_quality_50_and_sellin_negative_1_when_one_day_pass_given_quality_50_and_sellin_0_of_adebBrie_product_in_expiration_date() {
         AdedBrieProduct adebBrie = new AdedBrieProduct(50, 0);
         adebBrie.passDay();
         assertEquals(50, adebBrie.getQuality());
